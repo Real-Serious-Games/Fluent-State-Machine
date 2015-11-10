039d3eb01c0742f15e37d39b64cb513fedd6e8ca
Rory Dungan <rory.dungan@realseriousgames.com>
2015-11-10 15:38:34 +1000

    Added more failing tests

--- Tests/StateTests.cs
+++ Tests/StateTests.cs
@@ -52,5 +52,103 @@ namespace RSG.FluentStateMachineTests
             Assert.Equal(0, timesUpdateCalled);
             Assert.Equal(0, timesExitCalled);
         }
+
+        [Fact]
+        public void updating_root_state_updates_active_child()
+        {
+            var rootState = new State();
+
+            var childState = rootState.CreateChild("foo");
+            var timesUpdateCalled = 0;
+            childState.SetUpdateAction((state, dt) => timesUpdateCalled++);
+
+            rootState.ChangeState("foo");
+            rootState.Update(1.0f);
+
+            Assert.Equal(1, timesUpdateCalled);
+        }
+
+        [Fact]
+        public void push_state_exits_the_current_state()
+        {
+            var rootState = new State();
+
+            var childState = rootState.CreateChild("foo");
+            var timesExitCalled = 0;
+            childState.SetExitAction(state => timesExitCalled++);
+
+            var stateToPush = rootState.CreateChild("bar");
+
+            rootState.ChangeState("foo");
+            rootState.PushState("bar");
+
+            Assert.Equal(1, timesExitCalled);
+        }
+
+        [Fact]
+        public void push_state_enters_new_state()
+        {
+            var rootState = new State();
+
+            var childState = rootState.CreateChild("foo");
+            var stateToPush = rootState.CreateChild("bar");
+            var timesEnterCalled = 0;
+            stateToPush.SetEnterAction(state => timesEnterCalled++);
+
+
+            rootState.ChangeState("foo");
+            rootState.PushState("bar");
+
+            Assert.Equal(1, timesEnterCalled);
+        }
+
+        [Fact]
+        public void pop_state_exits_the_current_state()
+        {
+            var rootState = new State();
+
+            var childState = rootState.CreateChild("foo");
+            var timesExitCalled = 0;
+            childState.SetExitAction(state => timesExitCalled++);
+
+            rootState.ChangeState("foo");
+            rootState.PopState();
+
+            Assert.Equal(1, timesExitCalled);
+        }
+
+        [Fact]
+        public void pop_state_returns_to_previous_state_in_stack()
+        {
+            var rootState = new State();
+
+            var childState = rootState.CreateChild("foo");
+            var pushedState = rootState.CreateChild("bar");
+            var timesEnterCalled = 0;
+            childState.SetEnterAction(state => timesEnterCalled++);
+
+            rootState.ChangeState("foo");
+            rootState.PushState("bar");
+            rootState.PopState();
+
+            Assert.Equal(2, timesEnterCalled);
+        }
+
+        [Fact]
+        public void update_is_no_longer_called_on_deactivated_states()
+        {
+            var rootState = new State();
+
+            var childState = rootState.CreateChild("foo");
+            var timesUpdateCalled = 0;
+            childState.SetUpdateAction((state, dt) => timesUpdateCalled++);
+
+            rootState.ChangeState("foo");
+            rootState.Update(1.0f);
+            rootState.PopState();
+            rootState.Update(1.0f);
+
+            Assert.Equal(1, timesUpdateCalled);
+        }
     }
 }
