44da224f5db0b6038c731148bbf48e67ad70b55a
Rory Dungan <rory.dungan@realseriousgames.com>
2015-11-10 10:53:17 +1000

    First failing test.

--- Tests/StateTests.cs
+++ Tests/StateTests.cs
@@ -2,11 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Xunit;
 
 namespace RSG.FluentStateMachineTests
 {
     public class StateTests
     {
-        State testObject;
+        [Fact]
+        public void state_with_parent_is_added_to_parent()
+        {
+            var rootState = new State();
+
+            var childState = rootState.CreateChild("foo");
+
+            Assert.Equal(childState, rootState.Children["foo"]);
+        }
     }
 }
