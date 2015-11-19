864f8844a3819765d386f8ec9ed1a49359432f8b
Rory Dungan <rory.dungan@realseriousgames.com>
2015-11-19 16:57:26 +1000

    Started writing tests for StateBuilder

---
+++ Tests/StateBuilderTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace RSG.FluentStateMachineTests
+{
+    public class StateBuilderTests
+    {
+        [Fact]
+        public void state_adds_state_as_child_of_root_state()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void named_state_is_added_with_correct_name()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void enter_sets_onEnter_action()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void exit_sets_onExit_action()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void update_sets_onUpdate_action()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void condition_sets_action_for_condition()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void end_returns_parent_state()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
